7ffaee3d23a45a368c47a326e4bbf65266e598cd
bhardkri <bhardkri@sheridancollege.ca>
2025-10-30 08:59:38 -0400

    rev 1 local

---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Course.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermgitcode2.midtermgitcode2;
+
+/**
+ *
+ * @author krish
+ */
+public class Course {
+    
+    private String name;
+    private int number;
+    
+    public Course(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getNumber() {
+        return this.number;
+    }
+}
---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Student.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermgitcode2.midtermgitcode2;
+
+/**
+ *
+ * @author krish
+ */
+public class Student {
+    
+    private String name;
+    private int number;
+    
+    public Student(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getNumber() {
+        return this.number;
+    }
+}
